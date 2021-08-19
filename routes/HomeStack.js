2975465af31aad70f4862c473fa02042180ff0e8
Rishabh Singh <ris8645208@gmail.com>
2021-08-19 22:21:32 +0530

    new package drawer navigator added

--- routes/HomeStack.js
+++ routes/HomeStack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import {createDrawerNavigator} from '@react-navigation/drawer'
 import Home from "../screens/Home";
 import ReviewDetails from "../screens/ReviewDetails";
 
